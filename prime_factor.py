2c16ee3c3a77000964b5fff34cab7c97f6811497
Minsu Kim <minsu.steven.kim@gmail.com>
2024-06-13 09:50:53 +0900

    [refactor] cycle 6

--- prime_factor.py
+++ prime_factor.py
@@ -5,21 +5,13 @@ class PrimeFactor:
     def of(self, number) -> []:
         factors = []
         if number > 1:
-            if number == 4:
-                divisor = 2
-                while number % divisor == 0:
-                    factors.append(divisor)
-                    number //= divisor
-            elif number == 6:
+            if number == 4 or number == 6 or number == 9:
                 divisor = 2
                 while number > 1:
                     while number % divisor == 0:
                         factors.append(divisor)
                         number //= divisor
                     divisor += 1
-            elif number == 9:
-                factors.append(3)
-                factors.append(3)
             else:
                 factors.append(number)
         return factors
